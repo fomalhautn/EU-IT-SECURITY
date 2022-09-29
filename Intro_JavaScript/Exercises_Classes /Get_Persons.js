018ae466290a8520ca1a6b04821284857c96d665
Anastasiia Andriievska <79667606+fomalhautn@users.noreply.github.com>
2022-09-29 12:14:40 +0000

    add getperson.js

---
+++ Intro_JavaScript/Exercises_Classes /Get_Persons.js
@@ -0,0 +1,20 @@
+function getPersons() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+    return [
+        new Person("Maria", "Petterson", 22, "mp.gmail.com"),
+        new Person("Lexicon"),
+        new Person("Stephan", "Larsson", 25),
+        new Person("Peter", "Jansson", 24, "ptr@live.com")
+    ];
+}
+console.log(getPersons());
