b0d60a5ae6faf62b60868b7af5b05ca3e8d06823
Anastasiia Andriievska <79667606+fomalhautn@users.noreply.github.com>
2022-11-02 12:20:59 +0000

    django

--- DjangoLectureEnv/.venv/bin/Project_two/AppTwo/views.py
+++ DjangoLectureEnv/.venv/bin/Project_two/AppTwo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
+'''
 def index(request):
     return HttpResponse("<em>My Second App</em>")
 '''
@@ -14,4 +14,3 @@ def hello(request):
 def index(request):
     my_dict = {'insert_me': "Hello, i am from views.py !"}
     return render(request, 'second_app/index.html', context=my_dict)
-'''
